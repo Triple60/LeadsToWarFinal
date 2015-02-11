40b89b1f06e6889f6b6ddfc8a7f87069afbb50e1
Ethernetz <Ethannetz@hotmail.com>
2015-02-11 12:53:54 -0800

    Imported first files!

---
+++ LeadsToWarFinal.js
@@ -0,0 +1,147 @@
+    //This code was worked on primarily by Ayush Sharma.
+
+var cash = 10;
+
+var Unit = function(cost, level, attack, defense, speed, range, accuracy) {
+        this.cost = cost;
+        this.level = level;
+        this.attack = attack;
+        this.defense = defense;
+        this.speed = speed;
+        this.range = range;
+        this.accuracy = accuracy;
+        Unit.instances = 0;
+}
+
+function Rifleman(name) {
+    this.name = name;
+    this.riflemanNumber = "Rifleman" + riflemanArray.length
+    Unit.call(this, 25, 1, 10, 10, 10, 10, 10);    
+}
+
+function Tank(name) {
+    this.name = name;
+    this.tankNumber = "Tank" + tankArray.length
+    Unit.call(this, 100, 1, 25, 50, 5, 10, 5);
+}
+
+var unitArray = [];
+var player1UnitArray = [];
+var player2UnitArray = [];
+var riflemanArray = [];
+var tankArray = [];
+
+
+
+Rifleman.prototype = Object.create(Unit.prototype);
+Rifleman.prototype.constructor = Rifleman;
+Tank.prototype = Object.create(Unit.prototype);
+Tank.prototype.constructor = Tank;
+
+
+
+
+
+
+
+    /*this.levelUp = function() {
+        var toLevel = checkCash(Rifleman.cost);
+        this.level++;
+    };
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var Unit = function(price, health, power) {
+    this.price = price;
+    cash -= this.price;
+    this.fullHealth = health;
+    this.health = health;
+    this.power = power;
+    this.attack = function(prey) {
+        prey.health = prey.health - this.power;
+    };
+    this.healthPack = function() {
+        this.health = this.health + 50;
+        if (this.health > this.fullHealth) {
+            this.health = this.fullHealth;
+        }
+    };
+};
+
+var Infantry = function(price, health, power) {
+    this.price = price;
+    cash -= this.price;
+    this.fullHealth = health;
+    this.health = health;
+    this.power = power;
+    this.attack = function(prey) {
+        prey.health = prey.health - this.power;
+    };
+    this.healthPack = function() {
+        this.health = this.health + 50;
+        if (this.health > this.fullHealth) {
+            this.health = this.fullHealth;
+        }
+    };
+};
+
+var Vehicle = function(price, health, power) {
+    this.price = price;
+    cash -= this.price;
+    this.fullHealth = health;
+    this.health = health;
+    this.power = power;
+    this.attack = function(prey) {
+        prey.health = prey.health - this.power;
+    };
+    this.healthPack = function() {
+        this.health = this.health + 50;
+        if (this.health > this.fullHealth) {
+            this.health = this.fullHealth;
+        }
+    };
+};
+
+var Aircraft = function (price, health, power) {
+    this.price = price
+    cash -= this.price;
+    this.fullHealth = health;
+    this.health = health;
+    this.power = power;
+    this.attack = function(prey) {
+        prey.health = prey.health - this.power;
+        if (prey.health < 0) {
+            prey.health = 0;
+        }
+    };
+    this.healthPack = function() {
+        this.health = this.health + 50;
+        if (this.health > this.fullHealth) {
+            this.health = this.fullHealth;
+        }
+    };
+};*/
+
+/*var John = new Infantry(100, 50, 100);
+var Bob = new Vehicle(200, 100, 100);
+var Sam = new Aircraft(500, 150, 100);*/
---
+++ LeadsToWarFinalFunctions.js
@@ -0,0 +1,103 @@
+//This code was written by Elusch21//
+
+function checkCash(cost) {
+	if (cost > cash) return false; //FIX CASH PROBLEM. ADD TWO CASH SYSTEMS
+	else return true;
+} 
+
+var createRifleman = function(turn) {
+    if(checkCash(Rifleman.cost)) {
+		cash = cash - Rifleman.cost;
+    	var name = prompt("Whats the name?")
+    	var unitArraySize = unitArray.length;
+    	var riflemanArraySize = riflemanArray.length;
+    	window["Rifleman" + unitArraySize] = new Rifleman(name);
+    	unitArray.push(window["Rifleman" + unitArraySize]);
+    	riflemanArray.push(window["Rifleman" + riflemanArraySize]);
+    	if (turn===1) player1UnitArray.push(window["Rifleman" + unitArraySize]);
+    	else if (turn===-1) player2UnitArray.push(window["Rifleman" + unitArraySize]);
+    	addImage(Rifleman);
+    } else {
+    	alert("Not Enough Money!")
+    }
+}
+
+var createTank = function(turn) {
+	if(checkCash(Tank.cost)) {
+		cash = cash - Tank.cost;
+    	var name = prompt("Whats the name?")
+    	var unitArraySize = unitArray.length;
+    	var tankArraySize = tankArray.length;
+    	window["Tank" + unitArraySize] = new Tank(name);
+    	unitArray.push(window["Tank" + unitArraySize]);
+    	tankArray.push(window["Tank" + tankArraySize]);
+    	if (turn===1) player1UnitArray.push(window["Tank" + unitArraySize]);
+    	else if (turn===-1) player2UnitArray.push(window["Tank" + unitArraySize]);
+    	addImage(Tank);
+    } else {
+    	alert("Not Enough Money!")
+    }
+}
+
+
+	Unit.prototype.levelUp = function() {
+		if(checkCash(this.cost)) this.level ++;
+		else alert("Not Enough Money");
+	};
+
+	var attackUnit = function(unitId) {
+		var unitId = unitId.substr(7, 7);
+		if (turn===1) player2UnitArray[0].defense = player2UnitArray[0].defense - player1UnitArray[unitId-1].attack;
+		else if (turn===-1) player1UnitArray[0].defense =  player1UnitArray[0].defense - player2UnitArray[(switchFromP2(parseInt(unitId)) - 1) ].attack; 
+		
+		if (player2UnitArray[0].defense <= 0) {
+        	console.log(player2UnitArray[0].name + " is dead!");
+            removeImage(player2UnitArray[0].riflemanNumber);
+            player2UnitArray.splice(1, player2UnitArray.length); 
+            
+        } 
+
+        if (player1UnitArray[0].defense <= 0) {
+            console.log(player1UnitArray[0].name + " is dead!");
+            removeImage(player1UnitArray[0].riflemanNumber);
+            player1UnitArray.splice(1, player1UnitArray.length); 
+        } 
+
+        turn = turn * -1;
+	}
+
+
+    var switchFromP1 = function(unitId) {
+        if (unitId===1) return 8;
+        else if (unitId===2) return 7;
+        else if (unitId===3) return 6;
+        else if (unitId===4) return 5;
+        else if (unitId===5) return 4;  
+    }
+
+
+
+
+    var switchFromP2 = function(unitId) {
+        if (unitId===8) return 1;
+        else if (unitId===7) return 2;
+        else if (unitId===6) return 3;
+        else if (unitId===5) return 4;
+        else if (unitId===4) return 5;  
+    }
+
+
+	/*this.attackUnit = function() {
+        var i = true;
+        while (i == true) {
+            var toAttack = Math.floor(Math.random() * Units.length);
+            if (Units[toAttack].name !== this.name) {
+                i = false;
+            }
+        }
+        Units[toAttack].defense -= this.attack;
+        if (Units[toAttack].defense == 0) {
+            Units.splice(toAttack, 1);
+            removeImage(toAttack)
+        }
+    }*/
